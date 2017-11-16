f250b4316c7cc7f95586f790653f1a4df33bdee5
akasaMac <tarou_17n1101159@nnn.ed.jp>
2017-11-16 11:35:33 +0900

    Change the function name

--- Assets/3D_Project/Scripts/Charactor/CharaController.cs
+++ Assets/3D_Project/Scripts/Charactor/CharaController.cs
@@ -8,7 +8,7 @@ public class CharaController : MonoBehaviour {
 	Animator animator;
 	public CharaAttack child;
 	public Rigidbody rb;
-	float speed = 20.0f;
+	float speed = 10.0f;
 	public bool isGetHit = false;
 	bool isPressedUpKey = false;
 	bool isPressedDownKey = false;
@@ -28,58 +28,57 @@ public class CharaController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Move ();
-		ProcessKeys ();
+		Action ();
 
 	}
 		
-	void Move () {
+	void Action () {
 
-		
 		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.attack") == false) {
 
-			if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
-
-				// キー入力を値として取得
-				inputHorizontal = Input.GetAxisRaw ("Horizontal");
-				inputVertical = Input.GetAxisRaw ("Vertical");
-
-				// Cameraの正面方向をy軸を考慮せず取得
-				Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
-
-				// カメラベクトルに入力値を乗算してから正規化
-				Vector3 moveForward = ((cameraForward * inputVertical) + (Camera.main.transform.right * inputHorizontal)).normalized;
-
-				animator.SetBool ("run", true);
-
-				// 移動
-				rb.velocity = moveForward * speed;
-
-				// 方向転換
-				if (moveForward != Vector3.zero) {
-					transform.rotation = Quaternion.LookRotation(moveForward);
-				}
-
-			} else {
-				animator.SetBool ("run", false);
-				rb.velocity = Vector3.zero;
-			}
+			KeyEvents ();
 		}
 
-		if (animator.GetCurrentAnimatorStateInfo (0).normalizedTime >= 1.0f) {
-			child.attack = false;
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.attack")) {
+			if (animator.GetCurrentAnimatorStateInfo (0).normalizedTime >= 1.0f) {
+				child.attack = false;
+			}
 		}
 	}
 
-	void ProcessKeys () {
+	void KeyEvents () {
+		
 		if (Input.GetKeyDown ("space")) {
-			isPressedSpaceKey = true;
+			Debug.Log ("pass");
 			child.attack = true;
+			animator.SetTrigger ("attackTrigger");
 		}
 
-		if (isPressedSpaceKey) {
-			animator.SetTrigger("attackTrigger");
-			isPressedSpaceKey = false;
+		if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
+
+			// キー入力を値として取得
+			inputHorizontal = Input.GetAxisRaw ("Horizontal");
+			inputVertical = Input.GetAxisRaw ("Vertical");
+
+			// Cameraの正面方向をy軸を考慮せず取得
+			Vector3 cameraForward = Vector3.Scale (Camera.main.transform.forward, new Vector3 (1, 0, 1));
+
+			// カメラベクトルに入力値を乗算してから正規化
+			Vector3 moveForward = ((cameraForward * inputVertical) + (Camera.main.transform.right * inputHorizontal)).normalized;
+
+			animator.SetBool ("run", true);
+
+			// 移動
+			rb.velocity = moveForward * speed;
+
+			// 方向転換
+			if (moveForward != Vector3.zero) {
+				transform.rotation = Quaternion.LookRotation (moveForward);
+			}
+
+		} else {
+			animator.SetBool ("run", false);
+			rb.velocity = Vector3.zero;
 		}
 	}
 }
